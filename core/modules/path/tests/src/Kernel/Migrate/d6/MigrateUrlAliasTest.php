60ade51e14be4fd15adb8f64b507098eefc3a23f
Jenny Dumitrescu <jennifercumming07@gmail.com>
2018-04-26 09:34:48 +0100

    Update to drupal-8.5.3

--- core/modules/path/tests/src/Kernel/Migrate/d6/MigrateUrlAliasTest.php
+++ core/modules/path/tests/src/Kernel/Migrate/d6/MigrateUrlAliasTest.php
@@ -16,7 +16,7 @@ class MigrateUrlAliasTest extends MigrateDrupal6TestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = ['language', 'content_translation', 'path'];
+  public static $modules = ['language', 'content_translation', 'path', 'menu_ui'];
 
   /**
    * {@inheritdoc}
@@ -103,6 +103,14 @@ class MigrateUrlAliasTest extends MigrateDrupal6TestBase {
     ];
     $path = \Drupal::service('path.alias_storage')->load($conditions);
     $this->assertPath('3', $conditions, $path);
+
+    $path = \Drupal::service('path.alias_storage')->load(['alias' => '/source-noslash']);
+    $conditions = [
+      'source' => '/admin',
+      'alias' => '/source-noslash',
+      'langcode' => 'und',
+    ];
+    $this->assertPath('2', $conditions, $path);
   }
 
   /**
